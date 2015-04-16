2a81b99591153e87864974aeed1f41d62cdc308e
bgrove1974 <bgroveben@gmail.com>
2015-04-16 15:46:19 -0700

    Update test for posts controller

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -6,9 +6,13 @@ class PostsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get show" do
-    get :show
-    assert_response :success
+  test "should route to post" do
+    assert_routing '/posts/1', { :controller => "posts", :action => "show", :id => "1" }
   end
+
+#!#  test "should get show" do
+#!#    get :show
+#!#    assert_response :success
+#!#  end
 
 end
